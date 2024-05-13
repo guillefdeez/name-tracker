6d3700a58fa70cf5e012a11e91ebaecc3949860a
guillefdeez <guillermo.fernandez.283@gmail.com>
2024-05-13 13:09:11 +0200

    support for already existing file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 slint::include_modules!();
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{prelude::*, BufWriter};
+use std::path::Path;
+use std::fs::OpenOptions;
+
+
 
 fn main() -> Result<(), slint::PlatformError> {
   	let ui = MiVentana::new()?;
-  	let mut file = File::create_new("names.txt")
-		.expect("Error al crear archivo");
+	
+	match Path::new("names.txt").try_exists(){
+		Ok(false)=> {
+			let mut file = File::create_new("names.txt")
+				.expect("Error al crear archivo");
+			ui.on_save_name(move |string|{
+				let name: &[u8] = string.as_bytes();
+				file.write(name)
+					.expect("Error al escribir archivo");
+				file.write(b"\n").expect("Error al escribir al archivo");
+			});
+			
+		}
+		Ok(true)=>{
+			ui.on_save_name(move |string|{
+				let name: &[u8] = string.as_bytes();
+				let file = OpenOptions::new()
+					.append(true)
+					.open("names.txt")
+					.expect("Unable to open file");
+				let mut file = BufWriter::new(file);
+				file.write_all(name,).expect("Unable to write data");
+				file.write_all(b"\n").expect("Unable to write data");
+			});
 
-	ui.on_save_name(move |string|{
-		let name = string.as_bytes();
-		file.write(name)
-		.expect("Error al escribir archivo");
-		file.write(b"\n").expect("Error al escribir al archivo");
-	});
+		}
+		Err(_)=>println!("Error al comprobar si el archivo existe")
 
+	};
+  	
   	ui.run()
 }
